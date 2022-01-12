aec1a3188bf006ca425cd33ae2824d2aa86f5cf9
Sejalgupta20 <92423115+Sejalgupta20@users.noreply.github.com>
2022-01-12 21:06:42 +0530

    Add files via upload

---
+++ playground (2).swift
@@ -0,0 +1,10 @@
+class User {
+    internal init(firstName: String?, lastName: String?) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+    
+    let firstName: String?
+    let lastName: String?
+}
+
